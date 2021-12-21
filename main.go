92562aa0ed3ba39b6dde3f03431a5bf3e9ed90a9
enriquetejeda <quique.tejeda.68@gmail.com>
2021-12-21 13:23:23 -0500

    wip: add labels for searching containers

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/docker/docker/client"
 	"github.com/google/go-github/github"
 	"github.com/joho/godotenv"
+	log "github.com/sirupsen/logrus"
 )
 
 type config struct {
@@ -34,63 +35,93 @@ func initConfig() *config {
 }
 func init() {
 	if err := godotenv.Load(); err != nil {
-		log.Print("No .env file found")
+		log.Fatal("no .env file found")
 	}
 }
 func handleWebhook(w http.ResponseWriter, r *http.Request) {
 	payload, err := github.ValidatePayload(r, []byte(""))
 	if err != nil {
-		log.Printf("error validating request body: err=%s\n", err)
+		log.Error("error validating request body: err=%s\n", err)
 		return
 	}
 	defer r.Body.Close()
 
 	event, err := github.ParseWebHook(github.WebHookType(r), payload)
 	if err != nil {
-		log.Printf("could not parse webhook: err=%s\n", err)
+		log.Error("could not parse webhook: err=%s\n", err)
 		return
 	}
-	log.Printf("Received a webhook event!")
-
 	switch e := event.(type) {
 	case *github.PullRequestEvent:
 		if e.GetAction() == "closed" {
-			stopDocker(e)
+			handlePullRequestClosedEvent(e)
 		}
 	default:
-		log.Printf("unknown event type %s\n", github.WebHookType(r))
+		log.Error("unknown event type %s\n", github.WebHookType(r))
 		return
 	}
 }
-func replaceDotAndUppercase(repoName string) string {
-	return strings.ToUpper(strings.Replace(repoName, ".", "_", -1))
+
+func handlePullRequestClosedEvent(GithubEvent *github.PullRequestEvent) {
+	labels := filters.NewArgs(
+		filters.KeyValuePair{"projectName", replaceDotAndUppercase(GithubEvent.GetRepo().GetName())},
+		filters.KeyValuePair{"pullRequestNumber", strconv.Itoa(GithubEvent.GetPullRequest().GetNumber())}
+	)
+	err := stopContainer(labels)
+	if _, err != nil {
+		return err
+	}
 }
-func stopDocker(GithubEvent *github.PullRequestEvent) {
-	var nameContainer = "preview-" + replaceDotAndUppercase(GithubEvent.GetRepo().GetName()) + "-PR-" + strconv.Itoa(GithubEvent.GetPullRequest().GetNumber())
-	args := filters.NewArgs(filters.KeyValuePair{"name", string(nameContainer)})
+
+func replaceDotAndLowercase(repoName string) string {
+	return strings.ToLower(strings.Replace(repoName, ".", "_", -1))
+}
+
+func createPullRequestComment(GithubEvent *github.PullRequestEvent) (error) {
+	comment := "The container linked to this pull request has been stopped (%v)", GithubEvent.GetPullRequest().GetHTMLURL() + "."
+	if err := cli.Activity.CreateComment(ctx, GithubEvent.GetRepo().GetOwner().GetLogin(), GithubEvent.GetRepo().GetName(), GithubEvent.GetNumber(), &github.IssueComment{Body: &comment}); 
+	err != nil {
+		log.Error("error creating comment: err=%s\n", err)
+		return err
+	}
+	return nil
+}
+
+func findContainerByLabel(labels filters.Args) ([]types.Container, error) ) { 
 	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation(), client.WithHost("unix:///tmp/docker.sock"))
 	if err != nil {
-		panic(err)
+		return _, err
 	}
-
 	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{Filters: args})
 	if err != nil {
-		panic(err)
+		return _, err
 	}
+	log.Debug("Containers: %v", containers)
+	return containers, nil
+}
 
+func stopContainer(labels filters.Args) (listContainersStopped, error) {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation(), client.WithHost("unix:///tmp/docker.sock"))
+	if err != nil {
+		 return _, err
+	}
+	containers, err := findContainerByLabel(labels)
+	if err != nil {
+		return _, err
+	}
 	for _, container := range containers {
 		if err := cli.ContainerStop(ctx, container.ID, nil); err != nil {
-			panic(err)
+			return _, err
 		}
-		createComment(GithubEvent)
-		log.Printf("Stopping container %v...", container.ID[:10])
+		log.Debug("Stopping container %v...", container.ID[:10])
 	}
-
+	return listContainersStopped, nil
 }
-func main() {
 
-	log.Println("Server started..")
+func main() {
+	log.Debug("Server started..")
 	http.HandleFunc("/webhook", handleWebhook)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
